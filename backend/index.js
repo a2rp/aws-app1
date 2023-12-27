77acb7f2dc55a87792eaa86ea396882e7e3ce274
ash1198 <ash.ranjan09@gmail.com>
2023-12-28 00:42:21 +0530

    updated port

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.static(buildpath));
 
 console.log(path.join(_dirname, "frontend", "build"))
 
-const PORT = process.env.PORT || 1000;
+const PORT = process.env.PORT || 1198;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}: http://127.0.0.1:${PORT}`);
 });
